8caa94e0bb5aa016174febf221b6e7ce713e9b22
akclark <akclark@cryptospace.com>
2021-01-30 10:57:36 +0000

    arch slice 0

---
+++ app/controllers/customercontroller.js
@@ -0,0 +1,23 @@
+const db = require('../config/db.config.js');
+const Customer = db.Customer;
+
+exports.getCustomerById = (req, res) => {
+    // find all Customer information from 
+    let customerId = req.params.id;
+    Customer.findByPk(customerId)
+        .then(customer => {
+            res.status(200).json({
+                message: " Successfully Get a Customer with id = " + customerId,
+                customers: customer
+            });
+        })
+        . catch(error => {
+          // log on console
+          console.log(error);
+  
+          res.status(500).json({
+              message: "Error!",
+              error: error
+          });
+        });
+  }
---
+++ app/routes/routes.js
@@ -0,0 +1,4 @@
+let express = require('express');
+let router = express.Router();
+ 
+const customers = require('../controllers/customercontroller.js');
